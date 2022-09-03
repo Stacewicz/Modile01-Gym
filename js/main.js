0a091280e315ad89527e8a8bd25b21b23f3b2e17
Stacewicz <roman4fun89@gmail.com>
2022-09-03 20:35:12 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll(".banner__arrow, .nav_link");
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-700px";
+}
