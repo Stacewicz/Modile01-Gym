b057e40bcdb086cb586324da2298fb6d79a6848b
Stacewicz <roman4fun89@gmail.com>
2022-09-18 20:40:39 +0200

    change files

--- js/main.js
+++ js/main.js
@@ -18,3 +18,28 @@ function openModal() {
 function closeModal() {
     document.getElementById("modal").style.top = "-700px";
 }
+
+
+let inputs = document.querySelectorAll('input[data-rule]');
+
+for(let input of inputs) {
+    input.addEventListener('blur', function(){
+        let rule = this.dataset.rule;
+        let value =this.value;
+        let check;
+
+        switch(rule) {
+            case 'mail': 
+            check = /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([^<>()[].,;:\s@"]+.)+[^<>()[].,;:\s@"]{2,})/g.test(value);
+        }
+        this.classList.remove('invalid');
+        this.classList.remove('valid');
+        if (check) {
+            this.classList.add('valid');
+        } else {
+            this.classList.add('invalid');
+        } 
+
+
+    });
+}
